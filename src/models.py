833769f11e29753c2e62bd585aeb54064406a030
Juliana Monnerat Achiame <102694975+monneratachiame@users.noreply.github.com>
2024-03-18 11:14:01 +0000

    including new table people

--- src/models.py
+++ src/models.py
@@ -17,3 +17,23 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(80), unique=False, nullable=False)
+    hairColor = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hairColor": self.hairColor,
+            # do not serialize the password, its a security breach
+        }
+        
+        
